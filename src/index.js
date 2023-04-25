2aa9bc904638b2bb673bd4cd6c9b11059d78e1d4
Jay Lim <jlimoman@gmail.com>
2023-04-25 16:01:44 -0600

    add click event to heart glpyh & removed likes counter

--- src/index.js
+++ src/index.js
@@ -17,13 +17,6 @@ fetch(`${baseURL}/parks?api_key=${API_KEY}`)
 //! functions
 // card create function
 function createCard(obj) {
-    // add a count variable for each card's like button
-    let count = 0;
-
-    // create and add a <p> to hold like glyph
-    const likeContainer = document.createElement('p');
-    likeContainer.innerHTML = `<span class="heartGlyph">&#x2661;</span> ${count} likes!`
-
     // create card variable
     const card = document.createElement('div');
     card.className = "card";
@@ -61,7 +54,28 @@ function createCard(obj) {
     ${obj.addresses[0].line1}, ${obj.addresses[0].city}, ${obj.addresses[0].stateCode}, ${obj.addresses[0].postalCode}`;
     entranceFee.innerText = `Entrance fee: 
     ${obj.entranceFees[0].cost}`;
+
+    // add fee and location to card
+    // card.append(location, entranceFee);
     
+    // add a count variable for each card's like button
+    let count = 0;
+
+    // create and add a <p> to hold like glyph
+    const likeContainer = document.createElement('p');
+    likeContainer.innerHTML = `<span class="heartGlyph">&#x2661;</span>`;
+
+    // create 
+   
+    const glyphsArr = document.getElementsByClassName('heartGlyph')
+    //iterate over glpyhs array and attach listeners
+    for (let glyph of glyphsArr) {
+        glyph.addEventListener('click', e => {
+            // console.log(e.target);
+            count++;
+            e.target.innerHTML = '&#x2764;&#xfe0f;'
+        })
+    }
     // attach like button
     card.append(likeContainer);
     
@@ -95,7 +109,8 @@ const displayPark = (parkObj) => {
     parkAddress.innerText = `${parkObj.addresses[0].line1} \n ${parkObj.addresses[0].line2} \n ${parkObj.addresses[0].city}, ${parkObj.addresses[0].stateCode} ${parkObj.addresses[0].postalCode}`
 }
 
-//! Add event listeners 
+//! Add event listeners
+
 
 showFilters.addEventListener('click', e => {
     moreFilters.classList.toggle('hidden');
